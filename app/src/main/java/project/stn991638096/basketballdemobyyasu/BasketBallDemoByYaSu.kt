9ccbd0ceaaa35e15efdb1ba6321953f3d77a5fc2
su11ya <su11@sheridancollege.ca>
2023-02-14 10:35:53 -0500

    Initial commit

---
+++ app/src/main/java/project/stn991638096/basketballdemobyyasu/BasketBallDemoByYaSu.kt
@@ -0,0 +1,83 @@
+package project.stn991638096.basketballdemobyyasu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import project.stn991638096.basketballdemobyyasu.databinding.ActivityMainBinding
+
+
+class BasketBallDemoByYaSu : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    //create ViewModel instance scoreModel
+    private val scoreModel by lazy {
+        ViewModelProvider(this).get(ScoreViewModel::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        // display both teams scores
+        displayForTeamA(scoreModel.scoreTeamA)
+        // displayForTeamB(scoreModel.scoreTeamB)
+        displayForTeamB(scoreModel.scoreTeamB)
+
+    }
+
+
+    fun addOneForTeamA(v: View?) {
+        scoreModel.scoreTeamA = scoreModel.scoreTeamA + 1
+        displayForTeamA(scoreModel.scoreTeamA)
+    }
+
+    fun addTwoForTeamA(v: View?) {
+        addOneForTeamA(v)
+        addOneForTeamA(v)
+    }
+
+    fun addThreeForTeamA(v: View?) {
+        addOneForTeamA(v)
+        addOneForTeamA(v)
+        addOneForTeamA(v)
+    }
+
+    fun resetScore(v: View?) {
+        scoreModel.scoreTeamA = 0
+        displayForTeamA(scoreModel.scoreTeamA)
+        scoreModel.scoreTeamB = 0
+        displayForTeamB(scoreModel.scoreTeamB)
+    }
+
+    fun addOneForTeamB(v: View?){
+        scoreModel.scoreTeamB = scoreModel.scoreTeamB + 1
+        displayForTeamB(scoreModel.scoreTeamB)
+    }
+
+    fun addTwoForTeamB(v: View?) {
+        addOneForTeamB(v)
+        addOneForTeamB(v)
+    }
+
+    fun addThreeForTeamB(v: View?) {
+        addOneForTeamB(v)
+        addOneForTeamB(v)
+        addOneForTeamB(v)
+    }
+
+    // display score for TeamA
+    private fun displayForTeamA(score: Int) {
+        binding.teamAScore.text = score.toString()
+    }
+
+    //display score for TeamB
+    private fun displayForTeamB(score: Int){
+        binding.teamBScore.text = score.toString()
+    }
+
+}
---
+++ app/src/main/java/project/stn991638096/basketballdemobyyasu/ScoreViewModel.kt
@@ -0,0 +1,15 @@
+package project.stn991638096.basketballdemobyyasu
+
+import androidx.lifecycle.ViewModel
+
+class ScoreViewModel : ViewModel() {
+
+    //Tracks the score for Team A
+    var scoreTeamA = 0
+
+    //Tracks the score for Team B
+    var scoreTeamB = 0
+
+
+
+}
